4a12108782e898f7e7c4c5cf8644a4535e6e1c50
Nicoli <mncoli@3c22fbaabef6.ant.amazon.com>
2021-10-14 11:18:01 +0100

    updating index file

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          My Next.js Amplify app 
+          My Next.js Amplify app hello
         </h1>
       </main>
     </div>
